901742f652feb96814744e68069bd44e970256d7
samuel <gbadebosamuel23@gmail.com>
2024-04-02 15:29:08 -0700

    almost done

---
+++ script.js
@@ -0,0 +1,11 @@
+function auth() {
+    let money = Number(20000)
+
+}
+
+
+function getStarted(){
+    if (price.value == "" || product.value == "" || Quality.value =="") {
+        error.style.display = "block"
+    }
+}
